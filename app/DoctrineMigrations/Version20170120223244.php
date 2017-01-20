fd265bb674dda3e5b59412dd365d58e3a43f9836
Dejan Angelov <angelovdejan92@gmail.com>
2017-01-20 23:34:25 +0100

    friendship requests init

---
+++ app/DoctrineMigrations/Version20170120223244.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170120223244 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE friendship_request (id INT AUTO_INCREMENT NOT NULL, from_user_id INT NOT NULL, to_user_id INT NOT NULL, INDEX IDX_6CC48EE12130303A (from_user_id), INDEX IDX_6CC48EE129F6EE60 (to_user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE friendship_request ADD CONSTRAINT FK_6CC48EE12130303A FOREIGN KEY (from_user_id) REFERENCES user (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE friendship_request ADD CONSTRAINT FK_6CC48EE129F6EE60 FOREIGN KEY (to_user_id) REFERENCES user (id) ON DELETE CASCADE');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE friendship_request');
+    }
+}
---
+++ src/AppBundle/Entity/FriendshipRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="friendship_request")
+ */
+class FriendshipRequest
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @ORM\JoinColumn(name="from_user_id", referencedColumnName="id", nullable=false, onDelete="cascade")
+     */
+    private $fromUser;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @ORM\JoinColumn(name="to_user_id", referencedColumnName="id", nullable=false, onDelete="cascade")
+     */
+    private $toUser;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getFromUser() : User
+    {
+        return $this->fromUser;
+    }
+
+    public function setFromUser(User $fromUser)
+    {
+        $this->fromUser = $fromUser;
+    }
+
+    public function getToUser() : User
+    {
+        return $this->toUser;
+    }
+
+    public function setToUser($toUser) : User
+    {
+        $this->toUser = $toUser;
+    }
+}
