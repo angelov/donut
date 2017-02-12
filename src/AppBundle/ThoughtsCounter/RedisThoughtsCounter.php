274a4ac113152514203ee074ed4bf2e5f134ad7b
Dejan Angelov <angelovdejan92@gmail.com>
2017-02-12 21:51:14 +0100

    default value and logging if num of thoughts can't be fetched from redis

--- src/AppBundle/ThoughtsCounter/RedisThoughtsCounter.php
+++ src/AppBundle/ThoughtsCounter/RedisThoughtsCounter.php
@@ -4,14 +4,20 @@ namespace AppBundle\ThoughtsCounter;
 
 use AppBundle\Entity\User;
 use Predis\Client;
+use Predis\Connection\ConnectionException;
+use Predis\PredisException;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 
 class RedisThoughtsCounter implements ThoughtsCounterInterface
 {
     private $redisClient;
+    private $logger;
 
-    public function __construct(Client $redisClient)
+    public function __construct(Client $redisClient, LoggerInterface $logger = null)
     {
         $this->redisClient = $redisClient;
+        $this->logger = $logger ?: new NullLogger();
     }
 
     public function increase(User $user)
@@ -21,7 +27,13 @@ class RedisThoughtsCounter implements ThoughtsCounterInterface
 
     public function count(User $user): int
     {
-        $value = $this->redisClient->get($this->resolveKey($user));
+        try {
+            $value = $this->redisClient->get($this->resolveKey($user));
+        } catch (ConnectionException $exception) {
+            $this->logException($exception, $user);
+
+            return 0;
+        }
 
         return $value ? (int) $value : 0;
     }
@@ -29,5 +41,14 @@ class RedisThoughtsCounter implements ThoughtsCounterInterface
     private function resolveKey(User $user)
     {
         return sprintf('user_comments_%s', $user->getId());
+    }
+
+    private function logException(PredisException $exception, User $user)
+    {
+        $this->logger->alert(sprintf(
+            'Could not fetch number of thoughts for user [%d]: "%s"',
+            $user->getId(),
+            $exception->getMessage()
+        ));
     }
 }
