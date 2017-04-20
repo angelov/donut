c18a1e4ca6c69aa52b1f54705c74c99406423e1d
Dejan Angelov <angelovdejan92@gmail.com>
2017-04-20 17:08:00 +0200

    refactored the neo4j mutual friends resolver

---
+++ src/SocNet/Friendships/MutualFriendsResolver/Neo4jMutualFriendsResolver/IdsResolver.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace SocNet\Friendships\MutualFriendsResolver\Neo4jMutualFriendsResolver;
+
+use GraphAware\Neo4j\Client\Client;
+
+class IdsResolver
+{
+    private $client;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @return string[] array of ids of the mutual friends
+     */
+    public function findMutualFriends(string $firstId, string $secondId) : array
+    {
+        $query = '
+            MATCH
+                (first:User {id: {first}}),
+                (second:User {id: {second}}),
+                (y:User),
+                (first)-[:FRIEND]->(y),
+                (y)<-[:FRIEND]-(second)
+            RETURN
+                y.id
+        ';
+
+        $result = $this->client->run($query, [
+            'first' => $firstId,
+            'second' => $secondId
+        ]);
+
+        $ids = [];
+
+        foreach ($result->records() as $record) {
+            $ids[] = $record->get('y.id');
+        }
+
+        return $ids;
+    }
+}
