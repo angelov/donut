d39560701154d54dd5bb11a99e0ee03a33b4a222
Dejan Angelov <angelovdejan92@gmail.com>
2017-04-29 12:24:22 +0200

    sending email when a user registers

---
+++ src/AppBundle/EventSubscribers/Users/SendWelcomingEmail.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AppBundle\EventSubscribers\Users;
+
+use SocNet\Users\Events\UserRegisteredEvent;
+use Swift_Mailer;
+use Twig_Environment;
+
+// @todo add to behat features
+// @todo refactor
+class SendWelcomingEmail
+{
+    private $mailer;
+    private $twig;
+
+    public function __construct(Swift_Mailer $mailer, Twig_Environment $twig)
+    {
+        $this->mailer = $mailer;
+        $this->twig = $twig;
+    }
+
+    public function notify(UserRegisteredEvent $event) : void
+    {
+        $user = $event->getUser();
+
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Welcome to the family')
+            ->setFrom('send@example.com')
+            ->setTo($user->getEmail())
+            ->setBody(
+                $this->twig->render(
+                    'users/emails/welcome.html.twig', [
+                        'user' => $user
+                    ]
+                ),
+                'text/html'
+            );
+
+        $this->mailer->send($message);
+    }
+}
